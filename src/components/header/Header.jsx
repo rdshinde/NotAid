296a67d8152295c0212ee81f1b05b3a4f470db3a
Rishikesh Shinde <67017632+rdshinde@users.noreply.github.com>
2022-05-20 14:28:34 +0530

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,19 @@
 import styles from "./header.module.css";
 import { GiNotebook } from "../../services/icon-imports";
 import { Link } from "react-router-dom";
+import { useAuth } from "../../contexts";
 export const Header = () => {
+  const {
+    userAuthState: { isUserLoggedIn },
+  } = useAuth();
   return (
     <>
       <div className={`${styles.header_wrapper}`}>
         <div className={`${styles.header_logo_container}`}>
-          <Link to={`/home`} className={styles.link}>
+          <Link
+            to={isUserLoggedIn ? `/home` : "/auth/login"}
+            className={styles.link}
+          >
             <h1>
               <span className="text-default m-l-md">
                 <GiNotebook />
